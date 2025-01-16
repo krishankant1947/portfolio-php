af48d848304efdf00539fea657c87732d5e5f57f
krishankant1947 <kantk2492@gmail.com>
2025-01-16 11:53:54 +0530

    new file

---
+++ common/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+define('HOME', 'http://localhost/portfolio/');
+$path = [
+    'home' => HOME,
+    'post_login' => HOME.'login.php' 
+];
+
+function kk_site(){
+    return "Portfolio Manager";
+}
+
+function kk_get_url($name){
+    global $path;
+    
+    return $path[$name];
+}

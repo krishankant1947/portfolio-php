38ec500c6f9b2621a497d1d8d2ca1b3f73995ce0
krishankant1947 <kantk2492@gmail.com>
2025-01-17 20:04:01 +0530

    fgb

--- pages/main.php
+++ pages/main.php
@@ -25,6 +25,59 @@
                     </a>
                 </div>
             </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box text-bg-warning">
+                    <div class="inner">
+                        <h3>43</h3>
+                        <p>User Registrations</p>
+                    </div>
+                    <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"> more info
+                        <i class="bi bi-link-45deg"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box text-bg-danger">
+                    <div class="inner">
+                        <h3>65</h3>
+                        <p>Unique Visitors</p>
+                    </div>
+                    <a href="#" class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"> more info
+                        <i class="bi bi-link-45deg"></i>
+                    </a>
+                </div>
+            </div>
         </div><!--row-->
     </div><!--container-->
+    <div class="row">
+        <section class="col-lg-7 connectedSortable ui-sortable">
+            <div class="card">
+                <div class="card-header ui-sortable-handle" style="cursor: move;">
+                <h3 class="card-title">
+                  <i class="fas fa-chart-pie mr-1"></i>
+                  Sales
+                </h3>
+                <div class="card-tools">
+                  <ul class="nav nav-pills ml-auto">
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                    </li>
+                  </ul>
+                </div>
+                <ul class="nav nav-pills ml-auto">
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                    </li>
+                  </ul>
+                </div>
+            </div>
+        </section>
+        <section class=""></section>
+    </div><!--row-->
 </main>

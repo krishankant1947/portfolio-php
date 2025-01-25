9564ade076a263f7a0a9467a25ad8b490affa316
krishankant1947 <kantk2492@gmail.com>
2025-01-25 11:31:13 +0530

    LOGIN VALIDATION

--- login.php
+++ login.php
@@ -14,9 +14,12 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
       $array[]=("enter your data");
   }
   if(empty($array)){
-      $sql="insert into roles(title) value(?)";
+      $sql="insert into roles (title,password) values (:email,:password)";
       $stmt=$pdo->prepare($sql);
-      $stmt->execute([$_POST['email']]);
+      $stmt->execute([
+        'email' => $_POST['email'],
+        "password"=> password_hash($_POST['password'], PASSWORD_DEFAULT)
+      ]);
  };
  
 
@@ -31,12 +34,20 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         <div class="card-body login-card-body">
           <p class="login-box-msg">Sign in to start your session</p>
           <form action="<?php echo kk_get_url('post_login');?>" method="post">
+          <?php
+            if(!empty($array)){
+             echo '';
+             for($a=0 ; $a < count($array); $a++){
+              echo $array[$a];
+             }
+            }
+            ?>
             <div class="input-group mb-3">
               <input type="email" class="form-control" name="email" placeholder="Email" />
               <div class="input-group-text"><span class="bi bi-envelope"></span></div>
             </div>
             <div class="input-group mb-3">
-              <input type="password" class="form-control" placeholder="Password" />
+              <input type="password" class="form-control" name="password" placeholder="Password" />
               <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
             </div>
             <!--begin::Row-->
